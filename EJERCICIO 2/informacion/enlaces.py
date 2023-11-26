a0e30bdb75c3bef57fb4198ba846b979cd2ae8ae
carlospuigserver <cpuiggut@myuax.com>
2023-11-26 23:50:10 +0100

    c

--- EJERCICIO 2/informacion/enlaces.py
+++
@@ -1,8 +0,0 @@
-class Enlace:
-    def __init__(self, nombre, destino):
-        self.nombre = nombre
-        self.destino = destino
-        self.accesos = []
-
-    def acceder(self, usuario):
-        self.accesos.append((usuario, "Acceso"))
